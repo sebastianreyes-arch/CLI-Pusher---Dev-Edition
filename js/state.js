5b621aad6f888e026711f9b4df42fdfd51136fc5
sebastian <juansebastianrl2010@gmail.com>
2026-09-02 12:00:59 -0500

    anadiendo nuevos items parael jugador

--- js/state.js
+++ js/state.js
@@ -8,6 +8,8 @@
         copilot: { id: 'copilot', name: 'GitHub Copilot', cost: 1100, pps: 32, count: 0 },
         chatgpt: { id: 'chatgpt', name: 'ChatGPT Plus', cost: 12000, pps: 260, count: 0 },
         agent: { id: 'agent', name: 'Agente IA Autónomo', cost: 130000, pps: 1400, count: 0 },
-        claude: { id: 'claude', name: 'claude code', cost: 500000, pps: 5000, count: 0 }
+        claude: { id: 'claude', name: 'claude code', cost: 500000, pps: 5000, count: 0 },
+        torvals: { id: 'torvals', name: 'linus torvals', cost: 1000000, pps: 10000, count: 0 },
+        god: {id: 'god', name: 'god', cost: 20000000, pps: 1000000, count: 0},
       }
     };
